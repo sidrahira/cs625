a82484473121f6f0a4988bbb6550f5def5d2bf18
sidrahira <50233337+sidrahira@users.noreply.github.com>
2019-05-06 20:25:09 +0500

    git comment

---
+++ Example3.java
@@ -0,0 +1,16 @@
+public class Example3 {
+public static void main(String[] args ) {
+int var1;
+double x;
+var1=10;
+x=10.0;
+System.out.println("original value off variable ="+var1);
+
+System.out.println("original value off x ="+x);
+x=x/4;
+var1=var1/4;
+System.out.println("after dividing value off variable ="+var1);
+System.out.println("after dividing value off x ="+x);
+
+}
+}
---
+++ Fortest.java
@@ -0,0 +1,9 @@
+class Fortest{
+	publis static void main(String args[]);
+	throw java.io.IoException{
+		int i;
+		System.out.println("Press S to stop");
+		for(i=0;(char) System.in.read() !='S';i++)
+			System.out.println("Press#"+i);
+	}
+}
