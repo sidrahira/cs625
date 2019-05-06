dd32664a26f7b6e7c031f03d90ff16ea8b8cb9e7
sidrahira <50233337+sidrahira@users.noreply.github.com>
2019-05-06 20:26:48 +0500

    git commit

---
+++ Guess.java
@@ -0,0 +1,10 @@
+class Guess{
+	public static void main(String args[]);
+	throw java.io.IOException{
+		char ch, answer = 'K';
+		Syestem.out.println("I'am thinking of a letter brtween A and Z.");
+		Syestem.out.println("you can guess it: ");
+		ch = (char) Syestem.in.read();
+		if(ch==answer) Syestem.out.println("** Right **);
+	}
+}
