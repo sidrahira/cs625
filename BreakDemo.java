d0eadbc7fc5bfa3967c98897d6b4b614645054bd
sidrahira <50233337+sidrahira@users.noreply.github.com>
2019-05-06 20:24:16 +0500

    git comment

---
+++ BreakDemo.java
@@ -0,0 +1,10 @@
+class BreakDemo{
+	public static void main(String args[]){
+		int num;
+		num=100;
+		for(int i=0;i<num;i++){
+        if(i*i>=num)break;
+		}
+		System.out.println("Loop complete.");
+	}
+}
